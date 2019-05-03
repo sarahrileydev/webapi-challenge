0b87c0bbfd217e2e2a572555dc004d1caa5a8ed6
sriley <sarahriley.dev@gmail.com>
2019-05-03 13:50:19 -0400

    all CRUD operations

--- data/actionsRouter.js
+++ data/actionsRouter.js
@@ -15,7 +15,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-
 // get actions by ID
 router.get("/:id", async (req, res) => {
   try {
@@ -33,6 +32,57 @@ router.get("/:id", async (req, res) => {
     res
       .status(500)
       .json({ error: "The action information could not be retrieved." });
+  }
+});
+
+// post action
+router.post("/", async (req, res) => {
+  try {
+    const addAction = await Actions.insert(req.body);
+    res.status(200).json({ message: "New action added successfully." });
+  } catch (err) {
+    res.status(500).json({ message: "Server error. Action was not added." });
+  }
+});
+
+// update actions by ID
+router.put("/:id", async (req, res) => {
+  try {
+    const action = await Actions.get(req.params.id);
+
+    if (!action) {
+      res.status(404).json({
+        message:
+          "action not found."
+      });
+    } else {
+      await Actions.update(req.params.id, req.body);
+      res.json({ message: "Action updated" });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Server error."
+    });
+  }
+});
+
+
+
+//delete action
+router.delete("/:id", async (req, res) => {
+  try {
+    const count = await Actions.remove(req.params.id);
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res
+        .status(404)
+        .json({ message: "The action with the specified ID does not exist." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "The action could not be removed" });
   }
 });
 
--- data/projectsRouter.js
+++ data/projectsRouter.js
@@ -35,4 +35,53 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//post project
+router.post("/", async (req, res) => {
+  try {
+    const addProject = await Projects.insert(req.body);
+    res.status(200).json({ message: "New project added successfully." });
+  } catch (err) {
+    res.status(500).json({ message: "Server error. Project was not added." });
+  }
+});
+
+//update by ID
+router.put("/:id", async (req, res) => {
+  try {
+    const project = await Projects.get(req.params.id);
+
+    if (!project) {
+      res.status(404).json({
+        message:
+          "Project not found."
+      });
+    } else {
+      await Projects.update(req.params.id, req.body);
+      res.json({ message: "Action updated" });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Server error."
+    });
+  }
+});
+
+//delete project
+router.delete("/:id", async (req, res) => {
+  try {
+    const count = await Projects.remove(req.params.id);
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res
+        .status(404)
+        .json({ message: "The project with the specified ID does not exist." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "The project could not be removed" });
+  }
+});
+
 module.exports = router;
